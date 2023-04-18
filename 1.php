1bb3b82d809175967232fe7dc2ff0b6e1c87c0a7
Shanti Putri Syawalina <126060834+Shanti-Putri-Syawalina-012@users.noreply.github.com>
2023-04-18 12:18:00 +0700

    Update 1.php

--- 1.php
+++ 1.php
@@ -9,7 +9,7 @@
 	$conn = mysqli_connect("localhost","root","","mydb");
 
 	// Check connection
-	if (mysqli_connect_error()) {
+	if (mysqli_connect_errno()) {
 		echo "Failed to connect to MySQL: ". mysqli_connect_error();
 			exit();
 	}
